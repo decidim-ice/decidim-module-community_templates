aaea61bbdbc891bc4109a831695c264367422b43
Hadrien Froger <hadrien@octree.ch>
2025-09-22 16:48:21 +0100

    test: add tests to template_sources controller

--- spec/controllers/decidim/community_templates/admin/template_sources_controller_spec.rb
+++ spec/controllers/decidim/community_templates/admin/template_sources_controller_spec.rb
@@ -6,6 +6,9 @@ module Decidim::CommunityTemplates
   module Admin
     describe TemplateSourcesController do
       routes { Decidim::CommunityTemplates::AdminEngine.routes }
+      def reload_catalog
+        Decidim::CommunityTemplates::Catalog.from_path(Decidim::CommunityTemplates.catalog_path)
+      end
 
       let(:user) { create(:user, :confirmed, :admin, organization:) }
       let(:organization) { create(:organization) }
@@ -15,16 +18,90 @@ module Decidim::CommunityTemplates
         sign_in user, scope: :user
       end
 
+      describe "POST #create" do
+        let(:template_source) { build(:community_template_source, organization:) }
+        let(:participatory_process) { create(:participatory_process, organization:) }
+        let(:template_params) do
+          {
+            title: Faker::Lorem.sentence,
+            author: Faker::Name.name,
+            links: [Faker::Internet.url(scheme: "https")],
+            short_description: Faker::Lorem.sentence,
+            version: Faker::Lorem.sentence
+          }
+        end
+
+        it "creates a new template source" do
+          post :create, params: { template_source: { source_id: participatory_process.to_global_id.to_s, template: template_params } }
+          expect(response).to have_http_status(:redirect)
+          expect(Decidim::CommunityTemplates::TemplateSource.count).to eq(1)
+          expect(Decidim::CommunityTemplates::TemplateSource.last.source_id).to eq(participatory_process.id)
+          expect(Decidim::CommunityTemplates::TemplateSource.last.source_type).to eq("Decidim::ParticipatoryProcess")
+        end
+
+        it "write to catalog" do
+          post :create, params: { template_source: { source_id: participatory_process.to_global_id.to_s, template: template_params } }
+          expect(response).to have_http_status(:redirect)
+          catalog = reload_catalog
+          match = catalog.templates.find { |template| template.title == template_params[:title] }
+          expect(match).to be_present
+          expect(Decidim::CommunityTemplates.catalog_path.join(match.id)).to be_exist
+        end
+
+        it "define a new template id" do
+          expect do
+            post :create, params: { template_source: { source_id: participatory_process.to_global_id.to_s, template: template_params } }
+          end.to change(Decidim::CommunityTemplates::TemplateSource, :count).by(1)
+          expect(response).to have_http_status(:redirect)
+          catalog = reload_catalog
+          match = catalog.templates.find { |template| template.title == template_params[:title] }
+          expect(Decidim::CommunityTemplates::TemplateSource.last.template_id).to eq(match.id)
+        end
+
+        it "define default locale from organization" do
+          post :create, params: { template_source: { source_id: participatory_process.to_global_id.to_s, template: template_params } }
+          expect(response).to have_http_status(:redirect)
+          catalog = reload_catalog
+          match = catalog.templates.find { |template| template.title == template_params[:title] }
+          expect(match.default_locale).to eq(organization.default_locale)
+        end
+
+        context "when the template is not valid" do
+          let(:template_params) do
+            {
+              title: Faker::Lorem.sentence,
+              author: Faker::Name.name,
+              links: [Faker::Internet.url(scheme: "https")],
+              short_description: Faker::Lorem.sentence,
+              version: nil
+            }
+          end
+
+          it "does not create a new template source" do
+            expect do
+              post :create, params: { template_source: { source_id: participatory_process.to_global_id.to_s, template: template_params } }
+            end.not_to change(Decidim::CommunityTemplates::TemplateSource, :count)
+            expect(response).to have_http_status(:redirect)
+          end
+
+          it "render modal.js.erb if xhr?" do
+            post :create, params: { template_source: { source_id: participatory_process.to_global_id.to_s, template: template_params } }, xhr: true
+            expect(response).to have_http_status(:success)
+            expect(response).to render_template(partial: "decidim/community_templates/admin/template_sources/_modal")
+          end
+        end
+      end
+
       describe "PUT #update" do
         let(:template_source) { create(:community_template_source, organization:) }
         let(:id) { template_source.template_id }
         let(:template_params) do
           {
-            title: "Another Template",
-            author: "Another Author",
-            links: ["https://example.com"],
-            short_description: "Another Short Description",
-            version: "Another Version"
+            title: Faker::Lorem.sentence,
+            author: Faker::Name.name,
+            links: [Faker::Internet.url(scheme: "https")],
+            short_description: Faker::Lorem.sentence,
+            version: Faker::Lorem.sentence
           }
         end
 
@@ -34,17 +111,48 @@ module Decidim::CommunityTemplates
             template: template_params
           } }
           expect(response).to have_http_status(:redirect)
-          catalog = Decidim::CommunityTemplates::Catalog.from_path(Decidim::CommunityTemplates.catalog_path)
-          expect(catalog.templates.find { |template| template.id == id }.title).to eq("Another Template")
+          catalog = reload_catalog
+          expect(catalog.templates.find { |template| template.id == id }.title).to eq(template_params[:title])
         end
 
-        it "raises a 404 if the template is not found" do
-          expect do
-            put :update, params: { id: "not_found", template_source: {
+        context "when the template is not valid" do
+          let(:template_params) do
+            {
+              title: Faker::Lorem.sentence,
+              author: Faker::Name.name,
+              links: [Faker::Internet.url(scheme: "https")],
+              short_description: Faker::Lorem.sentence,
+              version: nil
+            }
+          end
+
+          it "raises a 404 if the template is not found" do
+            expect do
+              put :update, params: { id: "not_found", template_source: {
+                source_id: template_source.source.to_global_id.to_s,
+                template: template_params
+              } }
+            end.to raise_error(ActionController::RoutingError, "Not Found")
+          end
+
+          it "does not update the template" do
+            expect do
+              put :update, params: { id: id, template_source: {
+                source_id: template_source.source.to_global_id.to_s,
+                template: template_params
+              } }
+            end.not_to change(template_source, :updated_at)
+            expect(response).to have_http_status(:redirect)
+          end
+
+          it "render modal.js.erb if xhr?" do
+            put :update, params: { id: id, template_source: {
               source_id: template_source.source.to_global_id.to_s,
               template: template_params
-            } }
-          end.to raise_error(ActionController::RoutingError, "Not Found")
+            } }, xhr: true
+            expect(response).to have_http_status(:success)
+            expect(response).to render_template(partial: "decidim/community_templates/admin/template_sources/_modal")
+          end
         end
       end
     end
