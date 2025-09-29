48fa9e626bb2cb12451627501cff23f164a59507
Elvia Benedith <116598037+ElviaBth@users.noreply.github.com>
2025-09-29 12:19:31 +0200

    Add participatory process attributes (#24)
    * add participatory_process attributes
    
    * add more attributes
    
    * add spec checks
    
    * fix spec
    
    * add component attributes
    
    * add settings method
    
    * add more specs checks
    
    * remove some dates
    
    * remove method duplication
    
    ---------
    
    Co-authored-by: Ivan Vergés <ivan@pokecode.net>

--- app/services/decidim/community_templates/serializers/participatory_process.rb
+++ app/services/decidim/community_templates/serializers/participatory_process.rb
@@ -7,7 +7,22 @@ module Decidim
         def attributes
           {
             title: i18n_field(:title),
+            subtitle: i18n_field(:subtitle),
+            weight: model.weight,
+            slug: model.slug,
+            short_description: i18n_field(:short_description),
             description: i18n_field(:description),
+            announcement: i18n_field(:announcement),
+            start_date: model.start_date.iso8601,
+            end_date: model.end_date.iso8601,
+            developer_group: i18n_field(:developer_group),
+            local_area: i18n_field(:local_area),
+            meta_scope: i18n_field(:meta_scope),
+            target: i18n_field(:target),
+            participatory_scope: i18n_field(:participatory_scope),
+            participatory_structure: i18n_field(:participatory_structure),
+            private_space: model.private_space,
+            promoted: model.promoted,
             components:
           }
         end
